b42d1cdb5cc79cd233a24bd460a6e3efb41da4c7
Denesh Raymond <denesh.raymond@gmail.com>
2019-10-06 14:00:18 -0400

    Add MaterialApp and Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: _getText(),
+      ),
+    );
+  }
+
+  Widget _getText() {
     return Text(
       'Hello World!',
       textDirection: TextDirection.ltr,
